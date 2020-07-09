4898c4b3090254f715e496454ddf84d098e13884
buraksekili <buraksekili@gmail.com>
2020-07-09 14:48:28 +0300

    Updates

---
+++ precache-manifest.cd3203a60f9db5cb00a583ed07d16fd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe252cb009d5f4d2eeb5950e939a0c4",
+    "url": "/crop-img/index.html"
+  },
+  {
+    "revision": "29b6b65f5713fd9d057a",
+    "url": "/crop-img/static/js/2.b1dbdeef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crop-img/static/js/2.b1dbdeef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4374e230d55029775bab",
+    "url": "/crop-img/static/js/main.b2a751fe.chunk.js"
+  },
+  {
+    "revision": "3a1da15681941c62a818",
+    "url": "/crop-img/static/js/runtime-main.5f724ec2.js"
+  }
+]);
